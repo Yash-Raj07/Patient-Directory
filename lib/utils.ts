d5ae00409cf978fd656511ece157b9520e64c16e
Yash-Raj07 <sah99624@gmail.com>
2026-03-13 01:12:23 +0530

    fix(utils): ensure clsx output is correctly passed to tailwind-merge in cn helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,9 +2,5 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-
-  
-  clsx(inputs)
-
-  return twMerge(inputs as any)
+  return twMerge(clsx(inputs))
 }
